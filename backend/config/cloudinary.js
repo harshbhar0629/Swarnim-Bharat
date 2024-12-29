0c13893a91a47a658cbeeba783b415ce24a90eb4
harshbhar0629 <harshbhar.0629@gmail.com>
2024-12-29 18:40:11 +0530

    edit

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -7,7 +7,7 @@ exports.cloudinaryConnect = () => {
 		cloudinary.config({
 			// Configuring the Cloudinary to Upload MEDIA
 			cloud_name: process.env.CLOUD_NAME,
-			api_key: process.env.CLOUD_API_KEY,
+			api_key: process.env.CLOUD_API_KEY, 
 			api_secret: process.env.CLOUD_SECRET,
 		});
 		console.log("Cloudinary connect successfully!");
