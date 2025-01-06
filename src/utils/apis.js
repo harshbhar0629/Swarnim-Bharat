e34396ae7fa4b919c31bb57b876fae0a0c797117
harshbhar0629 <harshbhar.0629@gmail.com>
2025-01-06 21:08:20 +0530

    Edit backend path

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,10 +1,11 @@
 /** @format */
 
-const BASE_URL = "http://localhost:4000/api/v1";
+// const BASE_URL = "http://localhost:4000/api/v1";
+const BASE_URL = "https://swarnim-bharat-backend.vercel.app";
 
 // AUTH ENDPOINTS
 export const endpoints = {
 	SIGNUP_API: BASE_URL + "/auth/signup",
 	LOGIN_API: BASE_URL + "/auth/login",
-	CONTACT_US: BASE_URL + "/contact"
+	CONTACT_US: BASE_URL + "/contact",
 };
